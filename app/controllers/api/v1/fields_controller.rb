4c7f73f15434e63085d8355bd346727eb5fc7e37
Axel <maxelolsson@gmail.com>
2025-04-03 14:08:30 -0600

    reset git

--- app/controllers/api/v1/fields_controller.rb
+++ app/controllers/api/v1/fields_controller.rb
@@ -42,7 +42,7 @@ class Api::V1::FieldsController < Api::V1::BaseController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_project
       @project = Project.find(params[:project_id])
     end
